caf4d86e30bdd9a5ce85eec1e0d6f36aa9806986
Willis <willisdelamour@gmail.com>
2018-05-25 17:30:04 +0200

    lanceur

--- src/Equipe/Lanceur.java
+++ src/Equipe/Lanceur.java
@@ -1,16 +1,46 @@
 package Equipe;
 
 public class Lanceur {
-    int x,y;
-    public Lanceur(){
+
+
+    public static int posHome[][];// une position est affiche de tel maniere=> Case 1 : posHome[0]( x => "1", y => "2");
+    private int[] coord; //[0] = x et [1] = y et [2] = fruit (0 a 3 : c'est un fruit; 4 : c'est une chataigne; -1 : pas de fruit)
+
+
+    public Lanceur(int[] coord) {
+        this.coord = coord;
+    }
+
+    public String getAction() {
+        if (coord[2] >= 0) {
+            string x = homeThrow(coord);
+            if (x != null) {
+                return x;
+            } else {
+                return moveToHome();
+            }
+        } else {
+            String x = collect();
+            if (x != null) {
+                return x;
+            } else {
+                return //TODO appeler la fonction A*
+            }
+        }
+    }
+
+    public String moveToHome() {
+        //s'oriente sur l'axe soit vertical soit horizontal
+
 
     }
-    public String collect(){
-        int caseContent=(int)Client.map[x][y];
-        if(caseContent-'0'>=0&&caseContent-'0'<=3){
+
+
+    public String collect() {
+        int caseContent = (int) Client.map[x][y];
+        if (caseContent - '0' >= 0 && caseContent - '0' <= 3) {
             return "P";
-        }
-        else{
+        } else {
             return getClosestFruit();
         }
     }
