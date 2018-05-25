b52f0382a260a24b06e48f5fc01dc7772c3a10f2
depinfo <papajr@outlook.fr>
2018-05-25 16:27:37 +0200

    bla

--- src/Equipe/Lancer.java
+++ src/Equipe/Lancer.java
@@ -1,4 +1,13 @@
 package Equipe;
 
 public class Lancer {
+
+    checkClosestFruit(int[][] tabFruits, int[] posJoueur){
+        int i = 0;
+        boolean trouve = false;
+        while(i < tabFruits.length && !trouve){
+
+        }
+    }
+
 }
